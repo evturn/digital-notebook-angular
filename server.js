9480d2d9f6ca6500cab70db4d53ed846b3f4dbc3
Evan Turner <evturn@gmail.com>
2015-02-24 00:50:17 -0500

    server rendering index

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app			= express();
-var port    = process.env.PORT || 8080;
+var port    = process.env.PORT || 3000;
 var logger  = require('morgan');
 
 app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
+
+app.get('/', function(req, res) {
+	res.require('index.html')
+});
 
 app.listen(port);
 console.log("Express listening on localhost:" + port);
