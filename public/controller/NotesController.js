8b3884bac68984b50abc718b999ec1af099c9fac
Evan Turner <evturn@gmail.com>
2015-02-24 02:09:41 -0500

    Note Controller created

---
+++ public/controller/NotesController.js
@@ -0,0 +1,12 @@
+Guidebook.controller('AddNoteController',
+  function ($scope, $location, $routeParams, NoteModel) {
+    var chapterId = $routeParams.chapterId;
+ 		$scope.cancel = function() {
+    	$location.path('/chapter/' + chapterId);
+  	}
+  	$scope.createNote = function() {
+    	NoteModel.addNote(chapterId, $scope.note.content);
+     	$location.path('/chapter/' + chapterId);
+		} 
+	}
+);
