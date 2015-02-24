9d810ae9b7ade5112d34d762b923f3b0ddf3c297
Evan Turner <evturn@gmail.com>
2015-02-24 00:45:54 -0500

    express server created

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app			= express();
+var port    = process.env.PORT || 8080;
+var logger  = require('morgan');
+
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+
+app.listen(port);
+console.log("Express listening on localhost:" + port);
